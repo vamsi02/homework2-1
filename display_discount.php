e5bbab216fa60fbc4c37e47b07d3645e075a35c3
vamsi krishna surapaneni <vks35@afsconnect2.njit.edu>
2017-02-16 19:37:50 -0500

    Modify the first three echo statements

--- display_discount.php
+++ display_discount.php
@@ -1,7 +1,7 @@
 <?php
     $product_description = filter_input(INPUT_POST, 'product_description');
     $list_price = filter_input(INPUT_POST, 'list_price');
-    $discount_percent = filter_input(INPUT_post, 'discount_percent');
+    $discount_percent = filter_input(INPUT_POST, 'discount_percent');
 
 ?>
 <!DOCTYPE html>
@@ -15,13 +15,13 @@
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo htmlspecialchars($product_description); ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo htmlspecialchars($list_price); ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo htmlspecialchars($discount_percent); ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo ''; ?></span><br>
